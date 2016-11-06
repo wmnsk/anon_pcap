5e132791bcdcf3bc41433aec669842daab87467d
kurauchi-y <ahochauwaaaaa@gmail.com>
2016-11-06 20:58:00 +0900

    some refactorings

--- anon_pcap.py
+++ anon_pcap.py
@@ -4,31 +4,8 @@
 import argparse
 import binascii
 
-parser = argparse.ArgumentParser(description='Mini python script to replace specified value in PCAP(or any binary) file.')
-parser.add_argument('-s', '--srcpcap', help='Path to the raw PCAP file to be anonymized.', required=True)
-parser.add_argument('-d', '--dstpcap', help='Filename of the anonymized PCAP file.', default='anonymized.pcap')
-parser.add_argument('-v', '--values', help='A pair of values before/after anonymized.', metavar='VALUE', nargs=2, action='append')
-args = parser.parse_args()
-
-def strtohex(s):
-    h = binascii.hexlify(s)
-    return h
-
-def convert_valargs(vps):
-    vp = []
-    for l in vps:
-        vp.append(map(strtohex, l))
-    return vp
-
-def read_as_bin(path):
-    with open(path, 'rb') as infile:
-        binfile = infile.read()
-    return binfile
-
-def bintohex(b):
-    pcap_hex = binascii.hexlify(pcap_bin)
-
 def validate(vps):
+    '''Check the length of each argment in pairs. (Need better error handling...)'''
     for l in vps:
         if len(l[0]) != len(l[1]):
             print 'ERROR: Value to be replaced should be the same in length.'
@@ -36,34 +13,51 @@ def validate(vps):
             print '%s\t : %d' % (l[1], len(l[1]))
             quit(-1)
         else:
-            pass
+            return True
+
+parser = argparse.ArgumentParser(description='Mini python script to replace specified value in PCAP(or any binary) file.')
+parser.add_argument('-s', '--srcpcap', help='Path to the raw PCAP file to be anonymized.', required=True)
+parser.add_argument('-d', '--dstpcap', help='Filename of the anonymized PCAP file.', default='anonymized.pcap')
+parser.add_argument('-v', '--values', help='A pair of values before/after anonymized.', metavar='VALUE', nargs=2, action='append')
+args = parser.parse_args()
 
 src_pcap  = args.srcpcap
 dst_pcap  = open(args.dstpcap, 'w')
-val_pairs = convert_valargs(args.values)
+val_pairs = args.values
 
-class Pcap():
+class PcapData():
     'Main class for PCAP to be handled.'
     def __init__(self, srcpath, dstpath):
         self.dst = dstpath
-        self.pcap_bin = read_as_bin(srcpath)
-        self.pcap_hex = binascii.hexlify(self.pcap_bin)
+        self.pcap_bin = self.file2bin(srcpath)
+        self.pcap_hex = self.bin2hex(self.pcap_bin)
 
-    def replace_value(self, vps):
-        for l in vps:
-            self.pcap_hex = self.pcap_hex.replace(l[0], l[1])
-            print 'Replaced "%s" as "%s"' % (binascii.unhexlify(l[0]), binascii.unhexlify(l[1]))
-        return self.pcap_hex
+    def file2bin(self, path):
+        bindata = open(path, 'rb').read()
+        return bindata
 
-    def hex_to_pcap(self, hexlified):
-        self.pcap_mod_bin = bytearray.fromhex(hexlified)
-        self.dst.write(self.pcap_mod_bin)
+    def bin2hex(self, bindata):
+        hexdata = binascii.hexlify(bindata)
+        return hexdata
+
+    def str2hex(self, strdata):
+        h = binascii.hexlify(strdata)
+        return h
+
+    def replace_value(self, strargs):
+        for l in strargs:
+            self.pcap_mod_hex = self.pcap_hex.replace(self.str2hex(l[0]), self.str2hex(l[1]))
+            print 'Replaced "%s" as "%s"' % (l[0], l[1])
+        return self.pcap_mod_hex
+
+    def write_pcap(self, hexdata):
+        self.dst.write(bytearray.fromhex(hexdata))
 
 def main():
     validate(val_pairs)
-    p = Pcap(src_pcap, dst_pcap)
+    p = PcapData(src_pcap, dst_pcap)
     r = p.replace_value(val_pairs)
-    p.hex_to_pcap(r)
+    p.write_pcap(r)
 
 if __name__ == '__main__':
     main()
