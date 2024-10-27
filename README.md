35ccdccb7e7b7d073777e8eb3f702972ab90b527
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2024-10-27 21:09:44 +0100

    Python3 compat

--- README.md
+++ README.md
@@ -5,36 +5,33 @@ Initially developed to anonymize sensitive information in PCAP file, but this sc
 
 #### Disclaimer
 
-* Not working with Python3. Try installing Python2 or patching it by yourself (or contribute to this project!).
-* Beware of the unexpected changes made by this script. It is recommended to check the output PCAP file carefully.
+Beware of the unexpected changes made by this script.
+It is recommended to specify the as long value as possible, and to check the output PCAP file carefully.
 
 ## Features
 
 * Replace specified value in PCAP file.
-  * BCD-encoded values (e.g. IMSI, MSISDN, Global Title) can be replaced in string format (as copied as value on Wireshark).
-  * Any other field can be replaced in hex format (as copied as hexstream on Wireshark).
+  * Works for TBCD-encoded values (e.g. IMSI, MSISDN, Global Title).
+  * Any field can be replaced in hex format (as copied as hexstream on Wireshark).
 
 ## Usage
 
 ```shell-session
-# python anon_pcap.py -h
-usage: anon_pcap.py [-h] -s SRCPCAP [-d DSTPCAP] [-v STRVAL STRVAL]
-                    [-x HEXVAL HEXVAL]
+$ python3 ./anon_pcap.py -h
+usage: anon_pcap.py [-h] -s SRCPCAP [-d DSTPCAP] [-v STRVAL STRVAL] [-x HEXVAL HEXVAL]
 
 Mini python script to replace specified value in PCAP (or any binary) file.
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -s SRCPCAP, --srcpcap SRCPCAP
                         Path to the raw PCAP file to be anonymized.
   -d DSTPCAP, --dstpcap DSTPCAP
                         Filename of the anonymized PCAP file.
   -v STRVAL STRVAL, --strvals STRVAL STRVAL
-                        A pair of values before/after anonymized in STRING
-                        format.
+                        A pair of values before/after anonymized in STRING format.
   -x HEXVAL HEXVAL, --hexvals HEXVAL HEXVAL
-                        A pair of values before/after anonymized in HEX
-                        format.
+                        A pair of values before/after anonymized in HEX format.
 ```
 
 ## Examples
@@ -43,43 +40,31 @@ Replace [MCC/MNC information](https://github.com/wmnsk/mccmnc_scraper) in Diamet
 This example is expected to work on IMSI (in User-Name AVP or others if any) and Host/Realm.
 
 ```shell-session
-# python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
+$ python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
     -v 440000123456789 999990123456789 \
     -v epc.mnc000.mcc440.3gppnetwork.org epc.mnc999.mcc999.3gppnetwork.org
 ```
 
-Replace source/destination Global Titles and IMSI in SS7/SIGTRAN/etc.
+Replace source/destination Global Titles and IMSI in telco protocols (with BCD encoding).
+Note that you don't have to specify the swapped hex values in this case.
 
 ```shell-session
-# python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
-    -v 441481000001 441481009999 \ # Src GT or MSISDN(E.164)
-    -v 126800000001 126800009999 \ # Dst GT or MSISDN(E.164)
-    -v 23455000000001 23455000009999  # IMSI(E.212)
+$ python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
+    -v 441481000001 441481009999 \ # Some E.164 value
+    -v 126800000001 126800009999 \ # Some E.164 value
+    -v 23455000000001 23455000009999  # Some IMSI (E.212) value
 ```
 
 Replace MAC/IP addresses in any packets.  
-MAC/IP can only be replaced when specified in HEX string with "-x" keyword.
+They can only be replaced when specified in HEX string with "-x" keyword.
 
 ```shell-session
-# python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
+$ python anon_pcap.py -s path/to/src.pcap -d path/to/dst.pcap \
     -x 080042000001 080042ffffff \ # MAC Address in hex #1: "08:00:42:00:00:01" to "08:00:42:ff:ff:ff"
     -x 080042000002 080042999999 \ # MAC Address in hex #2: "08:00:42:00:00:02" to "08:00:42:99:99:99"
     -x deadbeef 6f6f6f6f # IP Address in hex #1: "222.173.190.239" to "111.111.111.111"
     -x beefdead dededede # IP Address in hex #2: "190.239.222.173" to "222.222.222.222"
-
 ```
-
-## Notes
-
-In some kind of "legacy" protocols, we sometimes need to be sure of its endianness. For instance, the IMSI (E.212 number) "440980123456789" in SS7/SIGTRAN PCAP is written as "44900821436587f9" in hexstream.  
-Currently, this script is tested on the following values in SIGTRAN packets and confirmed to work. Please create an issue if you had the unexpected results.
-
-| Fields | Human Readable | Actual Hex | Notes |
-| --- | --- | --- | --- |
-| IMSI | 440980123456789 | 44900821436587f9 | The last digit is filled with 'f' |
-| MSISDN | 81901234567 | 1809214365f7 | The last digit is filled with 'f' when the length is odd |
-| Global Title | 81901234567 | 180921436507 | The last digit is filled with '0' when the length is odd |
-
 
 ## Author
 
--- anon_pcap.py
+++ anon_pcap.py
@@ -1,22 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 '''
 anon_pcap.py - Mini python script to replace specified value in PCAP file.
 
-Copyright(C) 2016 Yoshiyuki Kurauchi
+Copyright(C) 2016-2024 Yoshiyuki Kurauchi
 License: MIT (https://github.com/wmnsk/anon_pcap/blob/master/LICENSE)
-Latest version is available on GitHub(https://github.com/wmnsk/anon_pcap).
+Latest version is available on GitHub (https://github.com/wmnsk/anon_pcap).
 '''
 
-from __future__ import print_function
 from binascii import hexlify as hx
 
 
 def validate_args(v, a):
-    '''Check the length of each argment in pairs.
-    TODO: Need better error handling...
-    '''
+    '''Check the length of each argument in pairs.'''
     if v is None and a is None:
         print('ERROR: No replace argument specified. Quitting.')
         quit(-1)
@@ -27,8 +24,6 @@ def validate_args(v, a):
                 print('%s\t : %d' % (x[0], len(x[0])))
                 print('%s\t : %d' % (x[1], len(x[1])))
                 quit(1)
-            else:
-                pass
 
     if a is not None:
         for x in a:
@@ -37,45 +32,41 @@ def validate_args(v, a):
                 print('%s\t : %d' % (x[0], len(x[0])))
                 print('%s\t : %d' % (x[1], len(x[1])))
                 quit(1)
-            else:
-                pass
 
 
-class PcapHandler(object):
-    '''Main class for PCAP to be handled.
-    TODO: Split the methods out for better implementation.
-    '''
+class PcapHandler:
+    '''Main class for PCAP to be handled.'''
     def __init__(self, srcpath, dstpath):
         self.dstfile = open(dstpath, 'wb')
         self.pcap_bin = self.file2bin(srcpath)
         self.pcap_hex = self.bin2hex(self.pcap_bin)
 
     def file2bin(self, path):
-        ''' read pcap file as binary. '''
+        '''read pcap file as binary.'''
         with open(path, 'rb') as d:
             bindata = d.read()
         return bindata
 
     def bin2hex(self, bindata):
-        ''' convert binary into hexadecimal. '''
-        hexdata = hx(bindata)
+        '''convert binary into hexadecimal.'''
+        hexdata = hx(bindata).decode('utf-8')
         return hexdata
 
     def str2hex(self, strdata):
-        ''' convert string into hexadecimal. '''
-        hexdata = hx(strdata)
+        '''convert string into hexadecimal.'''
+        hexdata = hx(strdata.encode('utf-8')).decode('utf-8')
         return hexdata
 
     def swap_str(self, strdata, fill):
-        ''' swap argument string by two characters.
-        (implemented for the legacy protocols like SS7). 
+        '''swap argument string by two characters.
+        (implemented for telco protocols with tbcd encoding).
         '''
         if len(strdata) % 2 == 1:
             strdata = strdata + fill
-        return ''.join(map(lambda x: x[::-1], [strdata[i: i+2] for i in range(0, len(strdata), 2)])) # python magic!
+        return ''.join(map(lambda x: x[::-1], [strdata[i: i+2] for i in range(0, len(strdata), 2)]))
 
     def replace_strval(self, strargs):
-        ''' replace value with string-formatted argument(-v). '''
+        '''replace value with string-formatted argument (-v).'''
         for l in strargs:
             orgstr = l[0]
             modstr = l[1]
@@ -83,30 +74,30 @@ class PcapHandler(object):
                 self.pcap_hex = self.pcap_hex.replace(
                     self.swap_str(orgstr, 'f'),
                     self.swap_str(modstr, 'f')
-                    )
+                )
                 self.pcap_hex = self.pcap_hex.replace(
                     self.swap_str(orgstr, '0'),
                     self.swap_str(modstr, '0')
-                    )
-                print('Replaced "%s" as "%s"' % (orgstr, modstr))
+                )
+                print(f'Replaced "{orgstr}" as "{modstr}"')
             else:
                 self.pcap_hex = self.pcap_hex.replace(
                     self.str2hex(orgstr), self.str2hex(modstr)
-                    )
-                print('Replaced "%s" as "%s"' % (orgstr, modstr))
+                )
+                print(f'Replaced "{orgstr}" as "{modstr}"')
         return self.pcap_hex
 
     def replace_hexval(self, hexargs):
-        ''' replace value with hex-formatted argument(-x). '''
+        '''replace value with hex-formatted argument (-x).'''
         for l in hexargs:
             orghex = l[0]
             modhex = l[1]
             self.pcap_hex = self.pcap_hex.replace(orghex, modhex)
-            print('Replaced "%s" as "%s"' % (orghex, modhex))
+            print(f'Replaced "{orghex}" as "{modhex}"')
         return self.pcap_hex
 
     def write_pcap(self, hexdata):
-        ''' write modified data as binary(pcap) file. '''
+        '''write modified data as binary (pcap) file.'''
         self.dstfile.write(bytearray.fromhex(hexdata))
         self.dstfile.close()
 
@@ -119,56 +110,52 @@ def test_diameter():
     str_pairs = [
         ['some00.node00.', 'some99.node99.'],
         ['mnc999.mcc999.', 'mnc000.mcc000.'],
-        ]
+    ]
     hex_pairs = [
         ['0b0b0b0b0b0b', '0a0a0a0a0a0a'],
         ['01010101', '6f6f6f6f']
-        ]
+    ]
 
     p = PcapHandler(org_path, tmp_path)
     r = p.replace_strval(str_pairs)
     r = p.replace_hexval(hex_pairs)
     p.write_pcap(r)
 
-    tmp = open('testpcaps/diameter_DWR_tmp.pcap', 'rb')
-    mod = open('testpcaps/diameter_DWR_mod.pcap', 'rb')
-    tmpfile = tmp.read()
-    modfile = mod.read()
+    with open('testpcaps/diameter_DWR_tmp.pcap', 'rb') as tmp, open('testpcaps/diameter_DWR_mod.pcap', 'rb') as mod:
+        tmpfile = tmp.read()
+        modfile = mod.read()
 
     assert (tmpfile == modfile)
-
-    tmp.close()
-    mod.close()
 
 
 def main():
     from argparse import ArgumentParser as AP
 
-    parser = AP(description='Mini python script to replace specified value in PCAP(or any binary) file.')
+    parser = AP(description='Mini python script to replace specified value in PCAP (or any binary) file.')
     parser.add_argument(
         '-s', '--srcpcap',
         help='Path to the raw PCAP file to be anonymized.',
         required=True
-        )
+    )
     parser.add_argument(
         '-d', '--dstpcap',
         help='Filename of the anonymized PCAP file.',
         default='anonymized.pcap'
-        )
+    )
     parser.add_argument(
         '-v', '--strvals',
         help='A pair of values before/after anonymized in STRING format.',
         metavar='STRVAL',
         nargs=2,
         action='append'
-        )
+    )
     parser.add_argument(
         '-x', '--hexvals',
         help='A pair of values before/after anonymized in HEX format.',
         metavar='HEXVAL',
         nargs=2,
         action='append'
-        )
+    )
     args = parser.parse_args()
 
     validate_args(args.strvals, args.hexvals)
@@ -183,8 +170,7 @@ def main():
 
 if __name__ == '__main__':
     from sys import argv
-    if argv[1] == 'test':
-    	# TODO: Add more tests.
+    if len(argv) > 1 and argv[1] == 'test':
         test_diameter()
     else:
         main()
